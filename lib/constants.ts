4faf5a5e790673732327c019ebb172ff7c2e3d53
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-03-17 00:41:21 +0300

    Change constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://geekday.live/';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'gtubt';
+export const BRAND_NAME = 'Bilgisayar Topluluğu';
+export const SITE_NAME_MULTILINE = ['Geekday', '2021'];
+export const SITE_NAME = 'Geekday';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'GTU Bilgisayar Topluluğu ekibinin düzenlediği 5.Geekday Etkinliği'
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+  'Biglisayar Geeklerinin Buluşma Adresi';
+export const DATE = '3-4 Nisan, 2021';
+export const SHORT_DATE = 'Nisan 3 - 13:45';
+export const FULL_DATE = 'Nisan 3 - 13:45';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -36,8 +36,8 @@ export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+  'https://www.notion.so/Code-of-Conduct-6b5c6f167d8f45bd9f999fc28f44b984';
+export const REPO = 'https://github.com/muhammedogz/geekday2021';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
