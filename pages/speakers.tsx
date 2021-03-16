58803e9fb64b4ef0f4f8bb26de77f82b2e8a362b
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-03-17 02:11:03 +0300

    Change Speakers Title

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -31,7 +31,7 @@ type Props = {
 
 export default function Speakers({ speakers }: Props) {
   const meta = {
-    title: 'Speakers - Virtual Event Starter Kit',
+    title: 'Konuşmacılar - Geekday',
     description: META_DESCRIPTION
   };
   return (
